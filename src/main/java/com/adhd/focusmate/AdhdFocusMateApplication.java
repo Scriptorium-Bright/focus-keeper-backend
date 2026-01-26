6e349ee19ed8579f8ce008a4415e45e447011801
notYourOcean <wjswjdgus333@gmail.com>
2026-01-26 13:09:17 +0900

    feat: 기본적인 프로젝트 구조 추가

---
+++ src/main/java/com/adhd/focusmate/AdhdFocusMateApplication.java
@@ -0,0 +1,13 @@
+package com.adhd.focusmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhdFocusMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdhdFocusMateApplication.class, args);
+    }
+
+}
