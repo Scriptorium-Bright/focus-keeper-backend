f73ca95569953926b80c02186e684818c6e9baff
notYourOcean <wjswjdgus333@gmail.com>
2026-03-21 01:02:32 +0900

    refactor: 코호트와 퍼널 분석 구현을 Phase 11 범위에서 제거

--- src/main/java/com/focuskeeper/reboot/recovery/analytics/dto/GenerateCohortRetentionRequest.java
+++
@@ -1,9 +0,0 @@
-package com.focuskeeper.reboot.recovery.analytics.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record GenerateCohortRetentionRequest(
-        @NotBlank(message = "cohortDate는 비어 있을 수 없습니다.")
-        String cohortDate
-) {
-}
